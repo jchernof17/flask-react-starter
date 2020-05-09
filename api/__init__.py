a9be9500f57fb629f24691d89c3d4c1d6e7fbae9
jchernof17 <jordan.chernof@gmail.com>
2020-05-09 18:45:15 -0400

    add alembic to backend

--- api/__init__.py
+++ api/__init__.py
@@ -7,13 +7,18 @@ from datetime import datetime
 
 from flask import Flask, json, jsonify, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-from passlib.hash import sha256_crypt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+"""
+Migration usage: 
+flask db migrate -m "migration message"
+flask db upgrade
+"""
 
 import api.routes
